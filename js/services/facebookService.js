d0ebb1a3d4a272e523c0aea49a8fd75341b732de
yohav <yohav3@gmail.com>
2014-09-19 18:33:17 +0300

    asd

--- js/services/facebookService.js
+++ js/services/facebookService.js
@@ -6,7 +6,7 @@ psycholish.factory("facebookService", function () {
             failCallback);
     }
     var Init = function(){
-        openFB.init('1478282099110792');
+        openFB.init('1478282099110792','https://www.facebook.com/connect/login_success.html');
     }
 
     var GetInfo = function (successCallback,failCallback) {
